0ff772ff86a2cbeac13f824ffef25efaf0359b18
eidolonzx <eidolonzx@gmail.com>
2022-07-25 17:34:06 +0400

    chore: make initial script structure

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+import argparse
+from page_loader.downloader import downloader
+
 
 def main():
-    print('Hello world!')
+    app_desc = 'Downloads a web page and saves it to your hard drive.'
+    parser = argparse.ArgumentParser(description=app_desc)
+    parser.add_argument('url', help="Enter web page url")
+    parser.add_argument('-o', '--output', help='Enter folder path')
+    args = parser.parse_args()
 
+    result = downloader(args.url, args.output)
+    print(result)
 
 if __name__ == '__main__':
     main()
